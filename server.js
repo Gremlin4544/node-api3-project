3f59486d35fc6e65f42d5c69088a91d9258d77b7
Gremlin4544 <alve@mckethen.com>
2020-03-11 13:22:50 -0600

    update userRouter

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
-server.get('/', (req, res) => {
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
+
+
+server.use(express.json());
+server.use(logger);
+server.use(cors());
+
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
+
+server.get('/',  (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+
+  console.log(`Logger: ${req.method} to ${req.url} @ ${Date(Date.now()).toString()}`);
+
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,25 +1,63 @@
 const express = require('express');
 
+const userDb = require('./userDb');
+const postDb = require('../posts/postRouter');
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser(), (req, res) => {
   // do your magic!
+  userDb.insert(req.body)
+  .then(newUser => {
+    res.status(200).json(newUser);
+  })
+  .catch(error => {
+    res.status(500).json({error: 'cannot add user'});
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUSerId, validate(), (req, res) => {
   // do your magic!
+  postDb.insert({...req.body, user_id: req.params.id})
+  .then(newPost => {
+    res.status(200).json(newPost);
+  })
+  .catch(error => {
+    res.status(500).json({error: 'cannot add post'});
+  })
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  userDb.get()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(error => {
+    res.status(500).json({error: 'cannot get users'});
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  userDb.getById(req.params.id)
+  .then(user => {
+    res.status(200).json(user);
+  })
+  .catch(error => {
+    res.status(500).json({error: 'cannot get user'});
+  })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  userDb.getUserPosts(req.params.id)
+  .then(userPosts => {
+    res.status(200),json(userPosts);
+  })
+  .catch(error => {
+    res.status(500).json({error: 'cannot get user posts'})
+  })
 });
 
 router.delete('/:id', (req, res) => {
